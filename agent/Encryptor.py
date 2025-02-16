9e5ff32a03b6381f1ac1ba5b428fe83fedc1f718
GavrielVax <3218087@gmail.com>
2025-02-16 16:43:52 +0200

    finish Encryptors

---
+++ agent/Encryptor.py
@@ -0,0 +1,20 @@
+from I_Encryptor import Encryptor
+
+
+class XorEncryptor(Encryptor):
+    """הצפנה בשיטת XOR על פי המפתח שהוזן"""
+
+    def encrypt(self) -> str:
+        """הצפנת הנתונים"""
+        result = []
+        if isinstance(self.key, int): #המפתח הינו מספר
+            for char in self.data:
+                result.append(chr(ord(char ^ self.key)))
+        else: # המפתח הינו מחרוזת
+            for i in range(len(self.data)):
+                result.append(chr(ord(self.data[i]) ^ ord(self.key[i % len(self.key)])))
+        return "".join(result)
+
+    def decrypt(self) -> str:
+        """פענוח הנתונים"""
+        return self.encrypt()
---
+++ agent/I_Encryptor.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Encryptor(ABC):
+    """תבנית להצפנת נתונים"""
+
+    def __init__(self, data, key: str | int):
+        """מקבל נתונים ומפתח להצפנה"""
+        self.data = data
+        self.key = key % 256 if isinstance(key, int) else key
+
+    @abstractmethod
+    def encrypt(self):
+        """הצפנת הנתונים"""
+        pass
+
+    @abstractmethod
+    def decrypt(self):
+        """פענוח הנתונים"""
+        pass
